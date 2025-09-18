b2839d2143d2e64a2156a0714bd1c09e4e30dd6b
Maria Falk <mariacelf@gmail.com>
2025-09-18 18:14:16 +0200

    Add bookingEntity

---
+++ Data/Entities/BookingEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.Entities
+{
+    public class BookingEntity
+    {
+        Guid Id { get; set; }
+        Guid MemberId { get; set; }
+        Guid WorkoutId { get; set; }
+        DateTime CreatedAt { get; set; }
+    }
+}
